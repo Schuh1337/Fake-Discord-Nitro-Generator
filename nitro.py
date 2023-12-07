1401f273c1b14e387d166fcd72cb79591446e74d
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-12-07 13:53:55 +0100

    Update nitro.py

--- nitro.py
+++ nitro.py
@@ -1,5 +1,4 @@
-import random
-import string
+import random, string
 while True:
     nitro = ''.join((random.choice(string.ascii_letters) for x in range(10)))
     print(f"Valid : discord.gift""/"+nitro)
